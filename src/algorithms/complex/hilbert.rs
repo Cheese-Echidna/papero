9d9e309afe210b20cd9900f778aeb4f6684716f4
Gabriel <gabrielgarriock@gmail.com>
2024-11-26 10:26:43 +0530

    Added Complex algorithms

---
+++ src/algorithms/complex/hilbert.rs
@@ -0,0 +1,53 @@
+use crate::*;
+use fast_hilbert;
+use image::GenericImage;
+
+#[derive(Default)]
+pub(crate) struct Hilbert;
+
+impl Generator for Hilbert {
+    fn generate(args: &Args) -> DynamicImage {
+        let (width, height) = (args.width, args.height);
+        if width > u16::MAX as u32 {
+            panic!("Too wide")
+        }
+
+        let mut image = RgbaImage::new(width, height);
+        for x in 0..width {
+            for y in 0..height {
+                image.put_pixel(x, y, Rgba([0,0,0,255]))
+            }
+        }
+
+        let dim = std::cmp::max(width, height);
+        // (10, 63), (12, 127), (20, 2047)
+        let order = (2.0 * ((dim as f64).log2() - 1.0)).ceil() as u8;
+
+        let total = 2.0_f64.powi(order as i32) - 1.0/2.0_f64.powi(order as i32);
+
+        let c = |t: f32| palette::Okhsl::new(t / total as f32 * 360., 1.0, 0.65);
+
+        let mut prev: Option<(u32, u32, u64)> = None;
+        for t in 0..=(total as u64) {
+            let (x1, y1) = fast_hilbert::h2xy::<u32>(t, order);
+            let (x,y) = (x1 * 2, y1 * 2);
+            if image.in_bounds(x,y) {
+                image.put_pixel(x, y, into(c(t as f32)));
+            }
+            if let Some((prev_x, prev_y, prev_t)) = prev {
+                let (mix_x, mix_y, mix_t) = ((x + prev_x)/2, (y+prev_y)/2, (t as f32 + prev_t as f32)/2.);
+                if image.in_bounds(mix_x,mix_y) {
+                    image.put_pixel(mix_x, mix_y, into(c(mix_t)));
+                }
+            }
+
+            prev = Some((x,y,t))
+        }
+
+        image.into()
+    }
+
+    fn name() -> &'static str {
+        "Hilbert Curve"
+    }
+}
--- src/algorithms/complex/mandel.rs
+++ src/algorithms/complex/mandel.rs
@@ -1,5 +1,9 @@
+use std::collections::HashSet;
 use num::complex::{Complex64, ComplexFloat};
+use palette::named::*;
+use palette::{convert, Clamp, IntoColor};
 use crate::*;
+use crate::utils::num_utils::lerp;
 
 #[derive(Default)]
 pub(crate) struct Mandel;
@@ -17,14 +21,11 @@ impl Generator for Mandel {
                 let (x,y) = ((px as f64 / width - 0.5), -((py as f64 / height - 0.5) * height / width));
                 let c = Complex64::new(x, y).scale(mandel_width) + centre;
 
-                let (dist_bound, iter_bound) = (2.0, 200);
+                let (dist_bound, iter_bound) = (2.0, 2000);
 
-                let esc = escape(c.recip(), dist_bound, iter_bound);
-                let i = esc.1;
+                let colour = escape_rgb(c.recip(), dist_bound, iter_bound);
 
-                let h = (i as f32 / iter_bound as f32).clamp(0.0, 1.0);
-                let colour = colour_utils::convert_from_ok_hsl(h*0.8, 1.0, steepstep(h));
-                image.put_pixel(px, py,colour);
+                image.put_pixel(px, py, colour);
             };
         };
 
@@ -39,17 +40,60 @@ impl Generator for Mandel {
 }
 
 fn escape(c: Complex64, escaped: f64, limit: u32) -> (Complex64, u32) {
+    let mut set = HashSet::new();
+
     let mut num = 0_u32;
     let mut z = c;
+    set.insert(bits(z));
+
+    let escaped_sqr = escaped.powi(2);
+
+    while z.norm_sqr() < escaped_sqr && num < limit {
+        if set.contains(&bits(z)) {
+            return (z, limit)
+        }
+        z = z.powu(2) + c;
+        num += 1;
+        set.insert(bits(z));
+    }
+
+    (c, num)
+}
+
+fn escape_rgb(c: Complex64, escaped: f64, limit: u32) -> Rgb<f32> {
+    let mut set = HashSet::new();
+
+    let mut num = 0_u32;
+    let mut z = c;
+    set.insert(bits(z));
 
     let escaped_sqr = escaped.powi(2);
 
     while z.norm_sqr() < escaped_sqr && num < limit {
         z = z.powu(2) + c;
         num += 1;
+        if set.contains(&bits(z)) {
+            let limit_prop = num as f32 / limit as f32;
+
+            return colour_utils::convert_from_ok_hsl(lerp(limit_prop, 1.0, 0.75), 1.0, 0.5);
+        }
+        set.insert(bits(z));
     }
 
-    (c, num)
+    let limit_prop = num as f32 / limit as f32;
+
+    if z.norm_sqr() >= escaped_sqr {
+        return colour_utils::convert_from_ok_hsl(0.8, 1.0, 1.0 - limit_prop);
+    }
+
+    Rgb([1.0, 1.0, 1.0])
+}
+
+
+fn bits(z: Complex64) -> u128 {
+    let b1 = z.re.to_bits();
+    let b2 = z.im.to_bits();
+    ((b1 as u128) << 64) | (b2 as u128)
 }
 
 fn smoothstep(x: f32) -> f32 {
